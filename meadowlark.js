3278b97b2934f2b0fbff11982c83c83db3aa518e
YOUQUE0 <leiyunran@qq.com>
2017-09-15 10:17:33 +0800

    Initial commit

---
+++ meadowlark.js
@@ -0,0 +1,69 @@
+var express = require('express');
+var app = express();
+
+app.use(express.static(__dirname + '/public'));
+
+// 设置handlebars视图引擎
+var handlebars = require('express3-handlebars').create({defaultLayout:'main'});
+app.engine('handlebars', handlebars.engine);
+app.set('view engine', 'handlebars');
+
+app.set('port', process.env.PORT || 3009);
+
+var fortunes = [
+	"Conquer your fears or they will conquer you.",
+	"Rivers need springs.",
+	"Do not fear what you don't know.",
+	"You will have a pleasnt surprise.",
+	"Whenever possible, keep it simple."
+]; 
+
+app.get('/', function(req, res){
+	res.render('home');
+});
+
+app.get('/about', function(req, res){
+	var m = Math.floor(Math.random() * fortunes.length);	
+	var randomFortune = fortunes[m];
+	console.log(randomFortune);
+	res.render('about', {fortune:randomFortune});
+});
+
+// roleType
+app.post('/gfwkkpds/getUserRoleTypesInPlan', function(req, res){
+	res.send({"result_code":"00","user_role_types":["03","02","03","04"]});
+});
+
+// 专项计划状态和基础资产类型
+app.post('/gfwkkpds/getSpecialPlanTypeAndStatus', function(req, res){
+	res.send({"result_code":"00","special_plan_base_info":{"asset_type":"0301","status":"60"}});
+});
+
+// 鉴权
+app.post('/aaa/authentication/gfwkSpecialPlanDetailInfo', function(req, res){
+	res.send({'result_code':'00'});
+});
+
+// 获取在所有计划的角色
+app.post('/gfwkkpds/getUserRoleTypes', function(req, res){
+	res.send({"result_code":"00","user_role_types":["03","02","03","04"]});
+});
+
+// 定制404页面
+app.use(function(req, res){	
+	res.status(404);
+	res.render('404');
+});
+
+// 定制500页面
+app.use(function(err, req, res, next){
+	console.error(err.stack);
+	//res.type('text/plain');
+	res.status(500);
+	res.render('500');
+});
+
+app.listen(app.get('port'), function(){
+	console.log('Express started on http://localhost:' + app.get('port') + ';press Ctrl-C to terminate');
+});
+
---
+++ test.js
@@ -0,0 +1,25 @@
+var express = require('express');
+var app = express();
+
+app.listen(3010, function(){
+	console.log('浏览器输入：http://localhost:3010');
+});
+
+app.get('/', function(req, res){
+	console.log(req.query);
+	test(5, function(v){
+		res.send({v});
+	});
+	
+	res.send({result_code:'00'});
+});
+
+var test = function(a, b, c){
+	console.log(typeof(b));
+	if(typeof(b) == 'function'){
+		c = b;
+		b = 'd';
+	}
+	console.log(typeof(b));
+	c(a);
+}
